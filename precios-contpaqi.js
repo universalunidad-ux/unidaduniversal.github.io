74c8ea06a72ca2c4ab45982d469210f272a14239
universalunidad-ux <universalunidad@gmail.com>
2025-10-14 19:55:06 -0600

    Create precios-contpaqi.js

---
+++ precios-contpaqi.js
@@ -0,0 +1,143 @@
+const preciosContpaqi = {
+  "CONTPAQi Contabilidad": {
+    "anual": {
+      "MonoRFC": { precio_base: 4190, usuario_adicional: 1590, renovacion: 3990 },
+      "MultiRFC": { precio_base: 5590, usuario_adicional: 1590, renovacion: 5190 }
+    },
+    "tradicional": {
+      actualizacion: { precio_base: 6990, usuario_adicional: 1690 },
+      especial: { precio_base: 6090, usuario_adicional: 1590 },
+      crecimiento_usuario: { usuario_adicional: 2390 }
+    }
+  },
+
+  "CONTPAQi Nóminas": {
+    "anual": {
+      "MonoRFC": { precio_base: 5190, usuario_adicional: 1590, renovacion: 4890 },
+      "MultiRFC": { precio_base: 6990, usuario_adicional: 1690, renovacion: 6490 }
+    },
+    "tradicional": {
+      actualizacion: { precio_base: 8190, usuario_adicional: 1990 },
+      especial: { precio_base: 7290, usuario_adicional: 1790 },
+      crecimiento_usuario: { usuario_adicional: 2390 }
+    }
+  },
+
+  "CONTPAQi Bancos": {
+    "anual": {
+      "MultiRFC": { precio_base: 6880, usuario_adicional: 1490, renovacion: 5990 }
+    },
+    "tradicional": {
+      actualizacion: { precio_base: 7290, usuario_adicional: 1590 },
+      especial: { precio_base: 6690, usuario_adicional: 1490 },
+      crecimiento_usuario: { usuario_adicional: 2390 }
+    }
+  },
+
+  "CONTPAQi XML en Línea": {
+    "anual": {
+      "MultiRFC": { precio_base: 1790, renovacion: 1690 }
+    }
+  },
+
+  "CONTPAQi Factura Electrónica": {
+    "anual": {
+      "MonoRFC": { precio_base: 2490, usuario_adicional: 890, renovacion: 2390 },
+      "MultiRFC": { precio_base: 3790, usuario_adicional: 1190, renovacion: 3590 }
+    },
+    "tradicional": {
+      actualizacion: { precio_base: 4690, usuario_adicional: 1390 },
+      especial: { precio_base: 3890, usuario_adicional: 1190 },
+      crecimiento_usuario: { usuario_adicional: 1790 }
+    }
+  },
+
+  "CONTPAQi Comercial START": {
+    "anual": {
+      "MonoRFC": { precio_base: 2490, usuario_adicional: 890, renovacion: 2390 },
+      "MultiRFC": { precio_base: 3790, usuario_adicional: 1190, renovacion: 3590 }
+    },
+    "tradicional": {
+      actualizacion: { precio_base: 4690, usuario_adicional: 1390 },
+      especial: { precio_base: 3890, usuario_adicional: 1190 },
+      crecimiento_usuario: { usuario_adicional: 1790 }
+    }
+  },
+
+  "CONTPAQi Comercial PRO": {
+    "anual": {
+      "MonoRFC": { precio_base: 8790, usuario_adicional: 2390, renovacion: 8490 },
+      "MultiRFC": { precio_base: 9790, usuario_adicional: 2590, renovacion: 9490 }
+    }
+  },
+
+  "CONTPAQi Comercial PREMIUM": {
+    "anual": {
+      "MultiRFC": { precio_base: 9490, usuario_adicional: 2490, renovacion: 9090 }
+    },
+    "tradicional": {
+      actualizacion: { precio_base: 12790, usuario_adicional: 3190 },
+      especial: { precio_base: 10590, usuario_adicional: 2590 },
+      crecimiento_usuario: { usuario_adicional: 3190 }
+    }
+  },
+
+  "CONTPAQi CFDI en línea": {
+    "anual": {
+      "MonoRFC": { precio_base: 1490 }
+    }
+  },
+
+  "CONTPAQi Evalúa": {
+    "nube": {
+      Inicial: { precio_base: 2090, usuarios_incluidos: "multi", empleados_incluidos: 15, empleado_adicional: 110 },
+      Equipos: { precio_base: 5890, usuarios_incluidos: "multi", empleados_incluidos: 50, empleado_adicional: 100 },
+      Empresarial: { precio_base: 9390, usuarios_incluidos: "multi", empleados_incluidos: 100, empleado_adicional: 70 },
+      Corporativo: { precio_base: 23090, usuarios_incluidos: "multi", empleados_incluidos: 500, empleado_adicional: 60 }
+    }
+  },
+
+  "CONTPAQi Despachos": {
+    "nube": {
+      Inicial: { precio_base: 4990, usuarios_incluidos: 3 },
+      Equipos: { precio_base: 7390, usuarios_incluidos: 5 },
+      Empresarial: { precio_base: 11190 },
+      Corporativo: { precio_base: 18990 },
+      usuario_adicional: 1690,
+      espacio_adicional: { "2GB": 1490, "5GB": 1990, "10GB": 2490 }
+    }
+  },
+
+  "CONTPAQi Contabiliza": {
+    "nube": {
+      Inicial: { precio_base: 4390, usuarios_incluidos: 1 },
+      Equipos: { precio_base: 5890, usuarios_incluidos: 2 },
+      Empresarial: { precio_base: 10990, usuarios_incluidos: 5 },
+      Corporativo: { precio_base: 19190, usuarios_incluidos: 10 },
+      usuario_adicional: 1790
+    }
+  },
+
+  "CONTPAQi Personia": {
+    "nube": {
+      Inicial: { precio_base: 3090, usuarios_incluidos: "multi", empleados_incluidos: 10, empleado_adicional: 180 },
+      Equipos: { precio_base: 4590, usuarios_incluidos: "multi", empleados_incluidos: 20, empleado_adicional: 120 },
+      Empresarial: { precio_base: 6090, usuarios_incluidos: "multi", empleados_incluidos: 50, empleado_adicional: 95 },
+      Corporativo: { precio_base: 8090, usuarios_incluidos: "multi", empleados_incluidos: 100, empleado_adicional: 45 }
+    }
+  },
+
+  "CONTPAQi Vende": {
+    "nube": {
+      LIGHT: { precio_base: 504, usuarios_incluidos: 1 },
+      Inicial: { precio_base: 1490, usuarios_incluidos: 1 },
+      Equipos: { precio_base: 2490, usuarios_incluidos: 2 },
+      Empresarial: { precio_base: 4990, usuarios_incluidos: 4 },
+      Corporativo: { precio_base: 10290, usuarios_incluidos: 8 },
+      usuario_adicional: 1490,
+      xml_historicos: { "250": 0, "500": 0, "1000": 0, "2500": 0, "5000": 0 },
+      addendas: true,
+      complementos: true
+    }
+  }
+};
